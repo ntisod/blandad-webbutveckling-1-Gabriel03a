c696f61b0b5b27b2d12b8c091a3016885c06828d
Gabriel03a <72020148+Gabriel03a@users.noreply.github.com>
2021-04-01 16:29:52 +0200

    Klar med Formulären

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -1,6 +1,29 @@
-function authorDialog()
-    alert("Gabriel har gjort den här filen 2022-03-22!");
+        /* En kommentar
+        som kan sträcka sig över flera rader. */
 
+        //En kommerntar på en rad.
+        function visaDialog(){
+            alert("Den här sidan är skapade av Gabriel, 2020-03-23!");
+            changeText();
+        }
 
+        function changeText(){
+            document.getElementById("demo").innerHTML = "Hello javaScript;"
+        }
+
+        function writeText(){
+            var namn = document.getElementById("fnamn").nodeValue;
+            document.getElementById("text").innerHTML = namn;
+        }
+
+        function bild(sw) {
+            var pic;
+            if (sw == 0) {
+              pic = "bild1.jpg."
+            } else {
+              pic = "bild2.jpg"
+            }
+            document.getElementById('myImage').src = pic;
+          }
 
 
