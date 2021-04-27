1d7dd10b67cd3d8f4998652f67fcaf9143be2e90
Gabriel03a <72020148+Gabriel03a@users.noreply.github.com>
2021-04-27 14:55:22 +0200

    ,

--- blanket.js
+++ blanket.js
@@ -1,7 +1,19 @@
-function showDiv() {
-    document.getElementById('Div1').style.display = "block";
- }
- 
- function showDiv2() {
-    document.getElementById('Div2').style.display = "block";
- }
+
+$(document).ready(function() {
+   $('select').change(function() {
+       if ($('select option[value="' + $(this).val() + '"]:selected').length > 1) {
+           $(this).val('-1').change();
+           alert('Du kan inte välja 2 av samma kurs.')
+       }
+   });
+});
+
+
+$(document).ready(function() {
+   $("div.answer_list").hide();
+   $("input[name$='answer']").click(function() {
+       var test = $(this).val();
+       $("div.answer_list").hide();
+       $("#" + test).show();
+   });
+});
